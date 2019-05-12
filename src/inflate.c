50ae8a11e1e69daefc18cafa987a19c2d06a3cab
Terrence Ho <terrenceho.books@gmail.com>
2019-05-11 21:40:40 -0700

    Redefined inflate using zlib

--- src/inflate.c
+++ src/inflate.c
@@ -940,45 +940,44 @@ static int inflate_block(int *e)
 }
 
 
-
-int
-inflate(void)
-/* decompress an inflated entry */
-{
-  int e;                /* last block flag */
-  int r;                /* result code */
-  unsigned h;           /* maximum struct huft's malloc'ed */
-
-
-  /* initialize window, bit buffer */
-  wp = 0;
-  bk = 0;
-  bb = 0;
+/* int*/
+/* inflate(void)*/
+/* /* decompress an inflated entry */*/
+/* {*/
+/*   int e;                /* last block flag */*/
+/*   int r;                /* result code */*/
+/*   unsigned h;           /* maximum struct huft's malloc'ed */*/
 
 
-  /* decompress until the last block */
-  h = 0;
-  do {
-    hufts = 0;
-    if ((r = inflate_block(&e)) != 0)
-      return r;
-    if (hufts > h)
-      h = hufts;
-  } while (!e);
-
-  /* Undo too much lookahead. The next read will be byte aligned so we
-   * can discard unused bits in the last meaningful byte.
-   */
-  while (bk >= 8) {
-    bk -= 8;
-    inptr--;
-  }
-
-  /* flush out slide */
-  flush_output(wp);
+/*   /* initialize window, bit buffer */*/
+/*   wp = 0;*/
+/*   bk = 0;*/
+/*   bb = 0;*/
 
 
-  /* return success */
-  Trace ((stderr, "<%u> ", h));
-  return 0;
-}
+/*   /* decompress until the last block */*/
+/*   h = 0;*/
+/*   do {*/
+/*     hufts = 0;*/
+/*     if ((r = inflate_block(&e)) != 0)*/
+/*       return r;*/
+/*     if (hufts > h)*/
+/*       h = hufts;*/
+/*   } while (!e);*/
+
+/*   /* Undo too much lookahead. The next read will be byte aligned so we*/
+/*    * can discard unused bits in the last meaningful byte.*/
+/*    */*/
+/*   while (bk >= 8) {*/
+/*     bk -= 8;*/
+/*     inptr--;*/
+/*   }*/
+
+/*   /* flush out slide */*/
+/*   flush_output(wp);*/
+
+
+/*   /* return success */*/
+/*   Trace ((stderr, "<%u> ", h));*/
+/*   return 0;*/
+/* }*/
--- src/unzip.c
+++ src/unzip.c
@@ -31,6 +31,8 @@
 #include <config.h>
 #include "tailor.h"
 #include "gzip.h"
+#include "zlib.h"
+#define CHUNK 16384
 
 /* PKZIP header definitions */
 #define LOCSIG 0x04034b50L      /* four-byte lead-in (lsb first) */
@@ -98,6 +100,67 @@ int check_zipfile(in)
 
     /* Get ofname and timestamp from local header (to be done) */
     return OK;
+}
+
+/* Inflate using zlib
+ */
+int inflate(void) 
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    /* ret = inflateInit(&strm); */
+    ret = inflateInit2(&strm, MAX_WBITS + 16);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 }
 
 /* ===========================================================================
@@ -215,3 +278,4 @@ int unzip(in, out)
     if (!test) abort_gzip();
     return err;
 }
+

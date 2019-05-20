491957e6590809924cc5dd53c3d2ec64a9b5fc7c
elihan27 <ehan2016@gmail.com>
2019-05-20 13:58:19 -0700

    removed recomputation

--- testing/bench.sh
+++ testing/bench.sh
@@ -1,105 +1,83 @@
 #!/bin/sh
 
+
 input=$(wc -c input.txt | sed 's/  */ /g' | cut -d " " -f 2)
-title="\tgzip\tbzip2\tlzma\n"
 
 printf "Input file size: $input\n"
 
+
+
+gzip_com=()
+gzip_dec=()
+bzip2_com=()
+bzip2_dec=()
+lzma_com=()
+lzma_dec=()
+
+
+
 printf "\nCompressed file size in bytes:\n"
-printf "$title"
+printf "\tgzip\tbzip2\tlzma\n"
 rm *.lzma *.gzip *bzip2 2>/dev/null 
 cp original.txt input.txt
-
 for i in `seq 1 9`;
- 	do
- 		cp original.txt input.txt
- 		gzip -$i input.txt
- 		compressed_size=$(wc -c input.txt.gz | sed 's/  */ /g' | cut -d " " -f 2)
- 		printf "$i\t$compressed_size"
- 		gzip -$i -d input.txt.gz
+do
+ 	
+	com_time=$((time gzip -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
+ 	compressed_size=$(wc -c input.txt.gz | sed 's/  */ /g' | cut -d " " -f 2)
+ 	dec_time=$((time gzip -$i -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
+ 	printf "$i\t$compressed_size"
+ 	gzip_com+=($com_time)
+ 	gzip_dec+=($dec_time)
 
- 		bzip2 -$i input.txt 
- 		compressed_size=$(wc -c input.txt.bz2 | sed 's/  */ /g' | cut -d " " -f 2)
- 		printf "\t$compressed_size"
- 		bzip2 -$i -d input.txt.bz2
+ 	com_time=$((time bzip2 -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
+ 	compressed_size=$(wc -c input.txt.bz2 | sed 's/  */ /g' | cut -d " " -f 2)
+ 	dec_time=$((time bzip2 -$i -d input.txt.bz2) 2>&1| sed '2q;d' | cut -f 2)
+ 	printf "\t$compressed_size"
+ 	bzip2_com+=($com_time)
+ 	bzip2_dec+=($dec_time)
 
- 		lzma -$i input.txt 
- 		compressed_size=$(wc -c input.txt.lzma | sed 's/  */ /g' | cut -d " " -f 2)
- 		printf "\t$compressed_size\n"
- 		lzma -$i -d input.txt.lzma
+ 	com_time=$((time lzma -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
+ 	compressed_size=$(wc -c input.txt.lzma | sed 's/  */ /g' | cut -d " " -f 2)
+ 	dec_time=$((time lzma -$i -d input.txt.lzma) 2>&1| sed '2q;d' | cut -f 2)
+ 	printf "\t$compressed_size\n"
+ 	lzma_com+=($com_time)
+ 	lzma_dec+=($dec_time)
 
-    done    
 
+
+done  
 
 printf "\nCompression time:\n"
-printf "\tgzip\t\tbzip2\t\tlzma\n"
+printf "\tgzip\tbzip2\tlzma\n"
 for i in `seq 1 9`;
- 	do
+do
+	j=($i)-1
+	printf "$i\t${gzip_com[$j]}"
+	printf "\t${bzip2_com[$j]}"
+	printf "\t${lzma_com[$j]}\n"
 
-
-		com_time=$((time gzip -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 		printf "$i\t$com_time"
- 		gzip -$i -d input.txt.gz
-
- 		com_time=$((time bzip2 -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 		printf "\t$com_time"
- 		bzip2 -$i -d input.txt.bz2
-
- 		com_time=$((time lzma -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 		printf "\t$com_time\n"
- 		lzma -$i -d input.txt.lzma
-
-
- 		#echo $i
-    done  
+done  
 
 printf "\nDecompression time:\n"
-printf "\tgzip\t\tbzip2\t\tlzma\n"
+printf "\tgzip\tbzip2\tlzma\n"
 for i in `seq 1 9`;
- 	do
+do
+	j=($i)-1
+	printf "$i\t${gzip_dec[$j]}"
+	printf "\t${bzip2_dec[$j]}"
+	printf "\t${lzma_dec[$j]}\n"
 
- 		gzip -$i input.txt
- 		dec_time=$((time gzip -$i -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
- 		printf "$i\t$dec_time"
-
- 		bzip2 -$i input.txt
- 		dec_time=$((time bzip2 -$i -d input.txt.bz2) 2>&1| sed '2q;d' | cut -f 2)
- 		printf "\t$dec_time"
-
- 		lzma -$i input.txt
- 		dec_time=$((time lzma -$i -d input.txt.lzma) 2>&1 | sed '2q;d' | cut -f 2)
- 		printf "\t$dec_time\n"
-
-
- 		#echo $i
-    done  
-
+done  
 
 rm *.lzma *.gzip *bzip2 2>/dev/null 
 
 
+ #if we're doing this, then we'll always need a compressed size, dec_size... * 3.  great.
 
 
 
-#gzip -d input.txt.gz   
-#com_time=$((time gzip input.txt) 2>&1 | sed '2q;d' | cut -f 2)
-#compressed_size=$(wc -c input.txt.gz | cut -d " " -f 6)
-#dec_time=$((time gzip -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
-
-#printf "compression time: \t$com_time\n"
-#printf "compressed file size: \t$compressed_size\n"
-#printf "decompression time: \t$dec_time\n"
 
 
 
-#com_time=$((time gzip input.txt) 2>&1 | sed '2q;d' | cut -f 2)
-#dec_time=$((time gzip -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
-#printf "compression time: \t$com_time\n"
-#printf "decompression time: \t$dec_time\n"
 
-#i=5
-#printf "and again:\n"
-#com_time=$((time gzip -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
-#dec_time=$((time gzip -$i -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
-
-#gzip -5d input.txt.gz

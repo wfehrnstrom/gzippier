3fc4da352cb4db46eee4793e45703fe84b32bd43
Ryan Alderete <ryanjalderete@ucla.edu>
2019-05-20 22:39:56 -0700

    Ensure compression code conforms to GNU style

--- src/zip.c
+++ src/zip.c
@@ -37,64 +37,68 @@ enum { SLOW = 2, FAST = 4 };
 
 /* Deflate using zlib
  */
-off_t deflateGZIP(int pack_level)
+off_t
+deflateGZIP (int pack_level)
 {
-    // source is input file descriptor, dest is input file descriptor
-    int ret, flush;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
-    int source = ifd;
-    int dest = ofd;
+  // source is input file descriptor, dest is input file descriptor
+  int ret, flush;
+  unsigned have;
+  z_stream strm;
+  unsigned char in[CHUNK];
+  unsigned char out[CHUNK];
+  int source = ifd;
+  int dest = ofd;
 
-    /* allocate deflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    ret = deflateInit2(
-            &strm,
-            pack_level,
-            Z_DEFLATED,         // set this for deflation to work
-            MAX_WBITS + 16,     // max window bits + 16 for gzip encoding
-            8,                  // memlevel default
-            Z_DEFAULT_STRATEGY  // strategy
-    );
-    if (ret != Z_OK)
-        return ret;
+  /* allocate deflate state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  ret = deflateInit2 (&strm,
+                      pack_level,
+                      Z_DEFLATED,     // set this for deflation to work
+                      MAX_WBITS + 16, // max window bits + 16 for gzip encoding
+                      8,              // memlevel default
+                      Z_DEFAULT_STRATEGY
+                      );
+  if (ret != Z_OK)
+    return ret;
 
-    /* compress until end of file */
-    do {
-        strm.avail_in = read(source, in, CHUNK);
-        if (errno != 0) {
-            (void)deflateEnd(&strm);
-            return Z_ERRNO;
+  /* compress until end of file */
+  do
+    {
+      strm.avail_in = read (source, in, CHUNK);
+      if (errno != 0)
+        {
+          (void)deflateEnd (&strm);
+          return Z_ERRNO;
         }
-        flush = (strm.avail_in != CHUNK) ? Z_FINISH : Z_NO_FLUSH;
-        strm.next_in = in;
+      flush = (strm.avail_in != CHUNK) ? Z_FINISH : Z_NO_FLUSH;
+      strm.next_in = in;
 
-        /* run deflate() on input until output buffer not full, finish
-           compression if all of source has been read in */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = deflate(&strm, flush);    /* no bad return value */
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            have = CHUNK - strm.avail_out;
-            if (write(dest, out, have) != have || errno != 0) {
-                (void)deflateEnd(&strm);
-                return Z_ERRNO;
+      /* run deflate() on input until output buffer not full, finish
+         compression if all of source has been read in */
+      do
+        {
+          strm.avail_out = CHUNK;
+          strm.next_out = out;
+          ret = deflate (&strm, flush);    /* no bad return value */
+          assert (ret != Z_STREAM_ERROR);  /* state not clobbered */
+          have = CHUNK - strm.avail_out;
+          if (write (dest, out, have) != have || errno != 0)
+            {
+              (void)deflateEnd (&strm);
+              return Z_ERRNO;
             }
         } while (strm.avail_out == 0);
-        assert(strm.avail_in == 0);     /* all input will be used */
+      assert (strm.avail_in == 0);     /* all input will be used */
 
-        /* done when last data in file processed */
+      /* done when last data in file processed */
     } while (flush != Z_FINISH);
-    assert(ret == Z_STREAM_END);        /* stream will be complete */
+  assert (ret == Z_STREAM_END);        /* stream will be complete */
 
-    /* clean up and return */
-    (void)deflateEnd(&strm);
-    return Z_OK;
+  /* clean up and return */
+  (void)deflateEnd (&strm);
+  return Z_OK;
 }
 
 /* ===========================================================================
@@ -102,21 +106,21 @@ off_t deflateGZIP(int pack_level)
  * IN assertions: the input and output buffers are cleared.
  *   The variables time_stamp and save_orig_name are initialized.
  */
-int zip(in, out)
-    int in, out;            /* input and output file descriptors */
+int
+zip(int in, int out)
 {
     /* uch  flags = 0;         /1* general purpose bit flags *1/ */
     /* ush  attr = 0;          /1* ascii/binary flag *1/ */
     /* ush  deflate_flags = 0; /1* pkzip -es, -en or -ex equivalent *1/ */
     /* ulg  stamp; */
 
-    ifd = in;
-    ofd = out;
+  ifd = in;
+  ofd = out;
     /* outcnt = 0; */
 
     /* Write the header to the gzip file. See algorithm.doc for the format */
 
-    method = DEFLATED;
+  method = DEFLATED;
     /* put_byte(GZIP_MAGIC[0]); /1* magic header *1/ */
     /* put_byte(GZIP_MAGIC[1]); */
     /* put_byte(DEFLATED);      /1* compression method *1/ */
@@ -160,9 +164,9 @@ int zip(in, out)
     /* header_bytes = (off_t)outcnt; */
 
 #ifdef IBM_Z_DFLTCC
-    dfltcc_deflate (level);
+  dfltcc_deflate (level);
 #else
-    deflateGZIP (level);
+  deflateGZIP (level);
 #endif
 
 #ifndef NO_SIZE_CHECK
@@ -181,7 +185,7 @@ int zip(in, out)
     /* header_bytes += 2*4; */
 
     /* flush_outbuf(); */
-    return OK;
+  return OK;
 }
 
 
@@ -190,21 +194,21 @@ int zip(in, out)
  * translation, and update the crc and input file size.
  * IN assertion: size >= 2 (for end-of-line translation)
  */
-int file_read(buf, size)
-    char *buf;
-    unsigned size;
+int
+file_read(char *buf, unsigned size)
 {
-    unsigned len;
+  unsigned len;
 
-    Assert(insize == 0, "inbuf not empty");
+  Assert (insize == 0, "inbuf not empty");
 
-    len = read_buffer (ifd, buf, size);
-    if (len == 0) return (int)len;
-    if (len == (unsigned)-1) {
-        read_error();
+  len = read_buffer (ifd, buf, size);
+  if (len == 0) return (int)len;
+  if (len == (unsigned)-1)
+    {
+      read_error();
     }
 
-    updcrc ((uch *) buf, len);
-    bytes_in += (off_t)len;
-    return (int)len;
+  updcrc ((uch *) buf, len);
+  bytes_in += (off_t)len;
+  return (int)len;
 }

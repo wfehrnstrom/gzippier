319e4680f4ec5d34eb85cb95f0eadb72c3242a1c
elihan27 <ehan2016@gmail.com>
2019-05-30 23:22:27 -0700

    tests gzip(pier) against old version of gzip

--- testing/bench.sh
+++ testing/bench.sh
@@ -4,12 +4,6 @@ file_size=10000000 #default size
 
 
 
-
-
-#set -e
-#set -u
-#set -o pipefail
-
 while getopts 's:' OPTION; do
   case "$OPTION" in
     s)
@@ -23,11 +17,19 @@ while getopts 's:' OPTION; do
 done
 shift "$(($OPTIND -1))"
 
+wget http://ftp.gnu.org/gnu/gzip/gzip-1.10.tar.xz
+tar xf gzip-1.10.tar.xz 
+cd gzip-1.10
+./configure
+make all
+mv gzip ../old_gzip
+cd ..
+
 
 echo "Generating input file..."
 head -c $file_size /dev/urandom >input.txt
 
-input=$(wc -c input.txt | sed 's/  */ /g' | cut -d " " -f 2)
+input=$(wc -c input.txt | sed 's/^[ \t]*//g' | cut -d " " -f 1)
 
 printf "Input file size: $input\n"
 
@@ -35,69 +37,61 @@ printf "Input file size: $input\n"
 
 gzip_com=()
 gzip_dec=()
-bzip2_com=()
-bzip2_dec=()
-lzma_com=()
-lzma_dec=()
+
+old_gzip_com=()
+old_gzip_dec=()
 
 
 
 printf "\nCompressed file size in bytes:\n"
-printf "\tgzip\tbzip2\tlzma\n"
-rm *.lzma *.gzip *bzip2 2>/dev/null 
+printf "\tgzip\told_gzip\n"
+rm *.lzma *.gz *bz2 2>/dev/null 
 #cp original.txt input.txt
 for i in `seq 1 9`;
 do
  	
 	com_time=$((time gzip -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 	compressed_size=$(wc -c input.txt.gz | sed 's/  */ /g' | cut -d " " -f 2)
+ 	compressed_size=$(wc -c input.txt.gz | sed 's/^[ \t]*//g' | cut -d " " -f 1)
  	dec_time=$((time gzip -$i -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
  	printf "$i\t$compressed_size"
  	gzip_com+=($com_time)
  	gzip_dec+=($dec_time)
 
- 	com_time=$((time bzip2 -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 	compressed_size=$(wc -c input.txt.bz2 | sed 's/  */ /g' | cut -d " " -f 2)
- 	dec_time=$((time bzip2 -$i -d input.txt.bz2) 2>&1| sed '2q;d' | cut -f 2)
- 	printf "\t$compressed_size"
- 	bzip2_com+=($com_time)
- 	bzip2_dec+=($dec_time)
-
- 	com_time=$((time lzma -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
- 	compressed_size=$(wc -c input.txt.lzma | sed 's/  */ /g' | cut -d " " -f 2)
- 	dec_time=$((time lzma -$i -d input.txt.lzma) 2>&1| sed '2q;d' | cut -f 2)
+ 	com_time=$((time ./old_gzip -$i input.txt) 2>&1 | sed '2q;d' | cut -f 2)
+ 	compressed_size=$(wc -c input.txt.gz | sed 's/^[ \t]*//g' | cut -d " " -f 1)
+ 	dec_time=$((time ./old_gzip -$i -d input.txt.gz) 2>&1| sed '2q;d' | cut -f 2)
  	printf "\t$compressed_size\n"
- 	lzma_com+=($com_time)
- 	lzma_dec+=($dec_time)
+	old_gzip_com+=($com_time)
+ 	old_gzip_dec+=($dec_time)
 
 
 
 done  
 
 printf "\nCompression time:\n"
-printf "\tgzip\tbzip2\tlzma\n"
+printf "\tgzip\t\told_gzip\n"
 for i in `seq 1 9`;
 do
 	j=($i)-1
 	printf "$i\t${gzip_com[$j]}"
-	printf "\t${bzip2_com[$j]}"
-	printf "\t${lzma_com[$j]}\n"
+	printf "\t${old_gzip_com[$j]}\n"
+
 
 done  
 
 printf "\nDecompression time:\n"
-printf "\tgzip\tbzip2\tlzma\n"
+printf "\tgzip\t\told_gzip\n"
 for i in `seq 1 9`;
 do
 	j=($i)-1
 	printf "$i\t${gzip_dec[$j]}"
-	printf "\t${bzip2_dec[$j]}"
-	printf "\t${lzma_dec[$j]}\n"
+	printf "\t${old_gzip_dec[$j]}\n"
 
 done  
 
-rm *.lzma *.gzip *bzip2 2>/dev/null 
+rm *.gz *bz2 2>/dev/null 
 rm input.txt
+rm -rf gzip-1.10* old_gzip
 
 
  #if we're doing this, then we'll always need a compressed size, dec_size... * 3.  great.
